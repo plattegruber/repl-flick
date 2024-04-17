47906f14265b39fcaacbc6a2948982a78ecb9ab7
Platte <gruberplatte@gmail.com>
2024-04-17 12:41:55 -0500

    add ghci support for haskell

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.Process (callCommand)
+
+launchRepl :: String -> IO ()
+launchRepl "haskell" = callCommand "ghci"
+launchRepl repl = error (repl ++ " not supported")
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Hello, Haskell!"
+  args <- getArgs
+  case args of
+    [repl] -> launchRepl repl
+    _ -> putStrLn "Usage: replflick <language>"
